b4adea7d181607b1885b299aea1d597bee3f6383
lukegledstone <lukegledstone@live.co.uk>
2022-07-18 17:13:45 +0100

    Added conditional examples and fixed indenting in calculator.

--- calculator.c
+++ calculator.c
@@ -3,7 +3,20 @@
 
 int main(void)
 {
-long x = get_long("x: ");
-long y = get_long("y: "); 
-printf("%li\n", x + y);
+    long x = get_long("x: ");
+    long y = get_long("y: "); 
+    printf("%li\n", x + y);
+}
+
+if (x < y)
+{
+	printf("x is less than y\n");
+}
+else if (x > y)
+{
+	printf("x is greater than y\n");
+}
+else
+{
+	printf("x is equal to y\n");
 }
