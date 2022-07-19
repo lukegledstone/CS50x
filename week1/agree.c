8752c523cf9db51d6c4844bb7013955d7b557f7f
lukegledstone <lukegledstone@live.co.uk>
2022-07-19 10:07:53 +0100

    Added comments on quotes usage

--- week1/agree.c
+++ week1/agree.c
@@ -7,12 +7,18 @@ int void(main)
     char c = get_char("Do you agree? (Y/N): ");
 
     // Check whether user agreed.
-    if (c == 'y')
+    if (c == 'y' || c == 'Y')
     {
         printf("Agreed.\n");
     }
-    else if (c == 'n')
+    else if (c == 'n' || c == 'N')
     {
         printf("Not agreed.\n");
     }
 }
+
+// In C, the way to check for 'or' is via two pipes - ||
+// The way to check for 'and' is via two ampersands - &&
+
+// Because the single character is being used, single quotes are required for the 'y' and 'N'. 
+// If it was a string data type, even if it was a single character, then "y" and "N" would be required.
