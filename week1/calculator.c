eb4726ed831cf048d754cd40233de936496440b1
lukegledstone <lukegledstone@live.co.uk>
2022-07-19 15:03:34 +0100

    Final changes of week1.

--- week1/calculator.c
+++ week1/calculator.c
@@ -3,20 +3,29 @@
 
 int main(void)
 {
-    long x = get_long("x: ");
-    long y = get_long("y: "); 
-    printf("%li\n", x + y);
-}
+    // Prompt user for x:
+    int x = get_float("x: ");
 
-if (x < y)
-{
-	printf("x is less than y\n");
-}
-else if (x > y)
-{
-	printf("x is greater than y\n");
-}
-else
-{
-	printf("x is equal to y\n");
+    // Prompt user for y:
+    int y = get_float("y: "); 
+
+    // Divide x by y
+    float z = (float) x / (float) y;
+
+    // Return z - use .Xf to return X decimal places when using float values. 
+    printf("%.2f\n", z);
+
+    // If using lots of significant places, if you only have 32 bits, you can get bit overflow when
+    // referencing decimal places as well. In this scenario, the computer will perform it's own 
+    // approximation of what the return value should be. 
+    // This can also be known as 'floating point inprecision'.
+    // This refers to the impossibility for computers to reference floating point values 100% accurately
+    // at least by default, in languages like C. 
+
+    // In languages like C, if you divide an integer by an integer, then you are returned an integer value.
+    // If you statically divide integers together, the decimal places are thrown away.
+    // Example:
+    //  int 2 / int 3 == 0
+    // Because the .666666 is dropped, as the values are integers. This is called truncation. 
+    // To get around this, you can type the x and y variables to be processed as (floats).
 }
